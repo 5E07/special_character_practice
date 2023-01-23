d67d9c681af5201561d4b43731a7a570116bdb37
5E07 <123369706+5E07@users.noreply.github.com>
2023-01-23 18:57:54 +0800

    All files you need

---
+++ special_character_practice.cpp
@@ -0,0 +1,35 @@
+//CreateSeed
+//RandomlyShuffle
+
+
+//srand(time(0));
+//random_shuffle(s.begin(), s.end());
+
+#define fre(x) freopen(#x,"r",stdin);
+#include<bits/stdc++.h>
+#include<time.h>
+using namespace std;
+char ans;
+int cnt;
+string s;
+
+int main(){
+    ifstream infile;
+    infile.open("raw.txt");
+    getline(infile,s);
+    srand(time(0));
+    random_shuffle(s.begin(), s.end());
+    for(int i=0;i<s.size();i++){
+        cout<<"Required input:"<<'\n';
+        cout<<s[i]<<'\n';
+        cout<<"Your input:"<<'\n';
+        std::cin>>ans;
+        if(s[i]==ans){
+            cnt++;
+        }
+    }
+    cout<<"\nAccuracy: "<<cnt<<'/'<<s.size()
+        <<" ("
+        <<setprecision(2)<<(float)cnt/s.size()
+        <<")\n";
+}
